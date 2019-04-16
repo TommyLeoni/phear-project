b6dc44cfe96a32664ee06cc3fbeee184dd65695e
Leoni Tomaso Aurelio Dominico <bleont@bbc-be.local>
2019-04-16 10:26:46 +0200

    Work on homepage and optimized structure

--- templates/user/index.php
+++ templates/user/index.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title><?= $title; ?></title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/stylesheet.css">
-    </head>
-    <body>
-        <h1 id="loool">ess arsch lolol</h1>
-    </body>
+
+<head>
+    <title><?= $title; ?></title>
+    <link rel="stylesheet" href="../style/css/stylesheet.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row text-center">
+            <div class="col-xs-5 col-sm-2 col-md-3">
+                <image src="../style/images/profile-pic.jpg" class="rounded" alt="Your profile picture"></image>
+            </div>
+            <div class="col-xs-10 col-sm-4 col-md-6">
+                TWO
+            </div>
+            <div class="col-xs-5 col-sm-2 col-md-3">
+                THREE
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
