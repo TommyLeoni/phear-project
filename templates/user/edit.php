4cd6701e8f6b7b6276b4492d7a6db70fae3272c9
Wüthrich Jouni Timo <bwuetj@bbc-be.local>
2019-04-16 10:28:02 +0200

    working on edit page

--- templates/user/edit.php
+++ templates/user/edit.php
@@ -1,1 +1,62 @@
-<h1>lol</h1>
+<html>
+<header>
+    <title> <?=$title;?> </title>
+</header>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-6 col-sm-4">
+                <h1>Profilbild</h1>
+            </div>
+            <div class="col-xs-12 col-sm-8">
+                <h1>lol</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-sm-4">
+                <h1>Benutzername</h1>
+            </div>
+            <div class="col-xs-12 col-sm-8">
+                <h1>lol</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-sm-4">
+                <h1>Email</h1>
+            </div>
+            <div class="col-xs-12 col-sm-8">
+                <h1>lol</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-sm-4">
+                <h1>Geburtsdatum</h1>
+            </div>
+            <div class="col-xs-12 col-sm-8">
+                <h1>lol</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-sm-4">
+                <h1>Bio</h1>
+            </div>
+            <div class="col-xs-12 col-sm-8">
+                <h1>lol</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6 col-sm-4">
+                <h1>Passwort</h1>
+            </div>
+            <div class="col-xs-12 col-sm-8">
+                <h1>lol</h1>
+            </div>
+        </div>
+        <div>
+            <Button></Button>
+        </div>
+    </div>
+</body>
+
+</html>
