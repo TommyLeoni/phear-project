91d4f41bea88ac91ffdbb7a5b4ad10438e1b470d
Wüthrich Jouni Timo <bwuetj@bbc-be.local>
2019-04-16 13:35:14 +0200

    working on edit.css and edit.php

--- templates/user/edit.php
+++ templates/user/edit.php
@@ -1,63 +1,61 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title> <?=$title;?> </title>
-    <link rel="stylesheet" href="/css/edit.css"/>
+    <link rel="stylesheet" href="/css/edit.css" />
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-6 col-sm-4">
-                <h1>Profilbild</h1>
+    <div class="container" >
+        <form method="POST">
+            <div>
+                <button type="button" size="auto" src="#Profilbild"></button>
             </div>
-            <div class="col-xs-12 col-sm-6">
-                <h1>lol</h1>
+            <div class="row">
+                <div class="col-xs-6 col-sm-4">
+                    <h1>Benutzername</h1>
+                </div>
+                <div class="col-xs-12 col-sm-8 middle">
+                    <input type="text" >
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-6 col-sm-4">
-                <h1>Benutzername</h1>
+            <div class="row">
+                <div class="col-xs-6 col-sm-4 ">
+                    <h1>Email</h1>
+                </div>
+                <div class="col-xs-12 col-sm-8 middle">
+                    <input type="email">
+                </div>
             </div>
-            <div class="col-xs-12 col-sm-8">
-                <h1>lol</h1>
+            <div class="row ">
+                <div class="col-xs-6 col-sm-4 ">
+                    <h1>Geburtsdatum</h1>
+                </div>
+                <div class="col-xs-12 col-sm-8 middle">
+                    <input type="date">
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-6 col-sm-4">
-                <h1>Email</h1>
+            <div class="row">
+                <div class="col-xs-6 col-sm-4 ">
+                    <h1>Bio</h1>
+                </div>
+                <div class="col-xs-12 col-sm-8 middle">
+                    <textarea></textarea>
+                </div>
             </div>
-            <div class="col-xs-12 col-sm-8">
-                <h1>lol</h1>
+            <div class="row">
+                <div class="col-xs-6 col-sm-4 ">
+                    <h1>Passwort</h1>
+                </div>
+                <div class="col-xs-12 col-sm-8 middle">
+                    <input type="password">
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-6 col-sm-4">
-                <h1>Geburtsdatum</h1>
+            <div>
+                <input type="submit" value="Ändern" class="btn btn-light middle">
             </div>
-            <div class="col-xs-12 col-sm-8">
-                <h1>lol</h1>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-6 col-sm-4">
-                <h1>Bio</h1>
-            </div>
-            <div class="col-xs-12 col-sm-8">
-                <h1>lol</h1>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-6 col-sm-4">
-                <h1>Passwort</h1>
-            </div>
-            <div class="col-xs-12 col-sm-8">
-                <h1>lol</h1>
-            </div>
-        </div>
-        <div>
-            <Button id="edit-button">Ändern</Button>
-        </div>
+        </form>
     </div>
 </body>
 
