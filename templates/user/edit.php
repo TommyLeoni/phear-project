13a65b19e91bd95c782b0cc2ae2cab85ebd91245
Wüthrich Jouni Timo <bwuetj@bbc-be.local>
2019-04-17 09:39:56 +0200

    does edit.php corrections

--- templates/user/edit.php
+++ templates/user/edit.php
@@ -7,37 +7,39 @@
 </head>
 
 <body>
-    <div class="container-fluid  profile-bg">
-        <form class="text-center ">
-            <div class="form-group full">
-                <div>
-                    <img id="profile-pic" src="/images/profile-pic.jpeg" alt="#">
+    <div class="container-fluid text-center">
+        <form class="row justify-content-md-center">
+            <div class="col-md-6 profile-bg">
+                <div class="form-group full">
+                    <div>
+                        <img id="profile-pic" src="/images/profile-pic.jpeg" alt="#">
+                    </div>
+                    <button name="Profilbildupdate" class="btn bluecolor">Profilbild ändern</button>
                 </div>
-                <button name="Profilbildupdate" class="btn bluecolor">Profilbild ändern</button>
+                <div class="form-group full">
+                    <label for="exampleInputUsername1" class="titles1">Username</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        placeholder="Username">
+                </div>
+                <div class="form-group full">
+                    <label for="exampleInputEmail1" class="titles1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                        placeholder="Email">
+                </div>
+                <div class="form-group full">
+                    <label class="titles1">Geburtsdatum</label>
+                    <input type="date" class="form-control" id="exampleInputDate">
+                </div>
+                <div class="form-group full">
+                    <label class="titles1">Informationen über dich</label>
+                    <textarea id="textarea1" placeholder="Bio"></textarea>
+                </div>
+                <div class="form-group full">
+                    <label for="exampleInputPassword1" class="titles1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary">Ändern</button>
             </div>
-            <div class="form-group full">
-                <label for="exampleInputUsername1" class="titles1">Username</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                    placeholder="Username">
-            </div>
-            <div class="form-group full">
-                <label for="exampleInputEmail1" class="titles1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                    placeholder="Email">
-            </div>
-            <div class="form-group full">
-                <label class="titles1">Geburtsdatum</label>
-                <input type="date" class="form-control" id="exampleInputDate">
-            </div>
-            <div class="form-group full">
-                <label class="titles1">Informationen über dich</label>
-                <textarea id="textarea1" placeholder="Bio"></textarea>
-            </div>
-            <div class="form-group full">
-                <label for="exampleInputPassword1" class="titles1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-            </div>
-            <button type="submit" class="btn btn-primary">Ändern</button>
         </form>
     </div>
 </body>
